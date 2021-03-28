9f5d6633bfb8b13d233b2795cb470d380e61a974
shamilsaeed <39285099+shamilsaeed@users.noreply.github.com>
2021-03-28 13:39:02 -0400

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,7 @@
+#LOSS
+
+def loss_function(real, pred):
+    #mask the 0s from padding
+    mask = 1 - np.equal(real, 0)
+    loss_ = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=real, logits=pred) * mask
+    return tf.reduce_mean(loss_)
